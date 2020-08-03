84e45dc0ae8928bd584dadc1fb8fa03500d36c82
Sciederrick <derrickmbarani@gmail.com>
2020-08-03 22:23:52 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // server.js
 // where your node app starts
+require('dotenv').config('env')
 
 // init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,14 +20,14 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/req_header_parser_microservice", function (req, res) {
+  res.json({ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT||60500, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
